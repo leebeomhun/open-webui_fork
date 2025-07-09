0c7fc08fae8798795ecc08e7a7dcf11eb3eca94a
BHLee <lbh3124@naver.com>
2025-07-09 23:55:17 +0900

    팝업 스타일 수정, 다시 보지 않기 버튼추가(공지사항 내용이 바뀌면 다시 출력되도록)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -546,6 +546,11 @@
 				tokenTimer = setInterval(checkTokenExpiry, 15000);
 
 				if (value) {
+					const dismissedHash = localStorage.getItem('dismissedPopupHash');
+					if (dismissedHash && dismissedHash === POPUP_HASH) {
+						return;
+					}
+
 					const popupDismissed = localStorage.getItem('popupDismissed');
 					if (popupDismissed) {
 						const dismissedTime = new Date(parseInt(popupDismissed, 10));
@@ -669,15 +674,18 @@
 	<link crossorigin="anonymous" rel="icon" href="{WEBUI_BASE_URL}/static/favicon.png" />
 </svelte:head>
 
-{#if showPopup}
-	<Popup
-		on:close={() => (showPopup = false)}
-		on:close-for-day={() => {
-			showPopup = false;
-			localStorage.setItem('popupDismissed', new Date().getTime().toString());
-		}}
-	/>
-{/if}
+<Popup
+	bind:show={showPopup}
+	on:close={() => (showPopup = false)}
+	on:close-for-day={() => {
+		showPopup = false;
+		localStorage.setItem('popupDismissed', new Date().getTime().toString());
+	}}
+	on:dismiss-permanently={() => {
+		showPopup = false;
+		localStorage.setItem('dismissedPopupHash', POPUP_HASH);
+	}}
+/>
 
 {#if loaded}
 	{#if $isApp}
